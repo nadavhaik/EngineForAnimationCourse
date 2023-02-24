f4bfb189d64cbde3ce7b5d135150bf92155653d4
MattanCohen <mc.3552883@gmail.com>
2023-02-24 16:38:45 +0200

    ...

--- tutorial/Project/BasicScene.cpp
+++ tutorial/Project/BasicScene.cpp
@@ -59,7 +59,6 @@ void BasicScene::Init(float fov, int width, int height, float near, float far)
     
      snakeMaterial = {std::make_shared<Material>("snakeMaterial", program)}; // empty snakeMaterial
      prizeMaterial = {std::make_shared<Material>("prizeMaterial", program)}; // empty apple material
-//    SetNamedObject(cube, Model::Create, Mesh::Cube(), snakeMaterial, shared_from_this());
 
     snakeMaterial->AddTexture(0, "textures/box0.bmp", 2);
     prizeMaterial->AddTexture(0, "textures/grass.bmp", 2); // TODO: change apple texture
@@ -91,7 +90,7 @@ void BasicScene::Init(float fov, int width, int height, float near, float far)
     Eigen::MatrixXd vertexNormals = Eigen::MatrixXd::Ones(6,3);
     Eigen::MatrixXd textureCoords = Eigen::MatrixXd::Ones(6,2);
     std::shared_ptr<Mesh> coordsys = std::make_shared<Mesh>("coordsys",vertices,faces,vertexNormals,textureCoords);
-    for(int i=0; i<16; i++) {
+    for(int i=0; i<1; i++) {
         AddToTail();
     }
 
@@ -118,76 +117,78 @@ void BasicScene::MouseCallback(Viewport* viewport, int x, int y, int button, int
     // note: there's a (small) chance the button state here precedes the mouse press/release event
 
     if (action == GLFW_PRESS) { // default mouse button press behavior
-        PickVisitor visitor;
-        visitor.Init();
-        renderer->RenderViewportAtPos(x, y, &visitor); // pick using fixed colors hack
-        auto modelAndDepth = visitor.PickAtPos(x, renderer->GetWindowHeight() - y);
-        renderer->RenderViewportAtPos(x, y); // draw again to avoid flickering
-        pickedModel = modelAndDepth.first ? std::dynamic_pointer_cast<Model>(modelAndDepth.first->shared_from_this()) : nullptr;
-        pickedModelDepth = modelAndDepth.second;
-        camera->GetRotation().transpose();
-        xAtPress = x;
-        yAtPress = y;
+        SwitchCamera();
 
-        // if (pickedModel)
-        //     debug("found ", pickedModel->isPickable ? "pickable" : "non-pickable", " model at pos ", x, ", ", y, ": ",
-        //           pickedModel->name, ", depth: ", pickedModelDepth);
-        // else
-        //     debug("found nothing at pos ", x, ", ", y);
-
-        if (pickedModel && !pickedModel->isPickable)
-            pickedModel = nullptr; // for non-pickable models we need only pickedModelDepth for mouse movement calculations later
-
-        if (pickedModel)
-            pickedToutAtPress = pickedModel->GetTout();
-        else
-            cameraToutAtPress = camera->GetTout();
+//        PickVisitor visitor;
+//        visitor.Init();
+//        renderer->RenderViewportAtPos(x, y, &visitor); // pick using fixed colors hack
+//        auto modelAndDepth = visitor.PickAtPos(x, renderer->GetWindowHeight() - y);
+//        renderer->RenderViewportAtPos(x, y); // draw again to avoid flickering
+//        pickedModel = modelAndDepth.first ? std::dynamic_pointer_cast<Model>(modelAndDepth.first->shared_from_this()) : nullptr;
+//        pickedModelDepth = modelAndDepth.second;
+//        camera->GetRotation().transpose();
+//        xAtPress = x;
+//        yAtPress = y;
+//
+//        // if (pickedModel)
+//        //     debug("found ", pickedModel->isPickable ? "pickable" : "non-pickable", " model at pos ", x, ", ", y, ": ",
+//        //           pickedModel->name, ", depth: ", pickedModelDepth);
+//        // else
+//        //     debug("found nothing at pos ", x, ", ", y);
+//
+//        if (pickedModel && !pickedModel->isPickable)
+//            pickedModel = nullptr; // for non-pickable models we need only pickedModelDepth for mouse movement calculations later
+//
+//        if (pickedModel)
+//            pickedToutAtPress = pickedModel->GetTout();
+//        else
+//            cameraToutAtPress = camera->GetTout();
     }
 }
 
 void BasicScene::ScrollCallback(Viewport* viewport, int x, int y, int xoffset, int yoffset, bool dragging, int buttonState[])
 {
     // note: there's a (small) chance the button state here precedes the mouse press/release event
-    Eigen::Matrix3f system = camera->GetRotation().transpose();
-    if (pickedModel) {
-        pickedModel->TranslateInSystem(system, {0, 0, -float(yoffset)});
-        pickedToutAtPress = pickedModel->GetTout();
-    } else {
-        camera->TranslateInSystem(system, {0, 0, -float(yoffset)});
-        cameraToutAtPress = camera->GetTout();
-    }
+//    Eigen::Matrix3f system = camera->GetRotation().transpose();
+//    if (pickedModel) {
+//        pickedModel->TranslateInSystem(system, {0, 0, -float(yoffset)});
+//        pickedToutAtPress = pickedModel->GetTout();
+//    } else {
+//        camera->TranslateInSystem(system, {0, 0, -float(yoffset)});
+//        cameraToutAtPress = camera->GetTout();
+//    }
 }
 
 void BasicScene::CursorPosCallback(Viewport* viewport, int x, int y, bool dragging, int* buttonState)
 {
-    if (dragging) {
-        Eigen::Matrix3f system = camera->GetRotation().transpose() * GetRotation();
-        auto moveCoeff = camera->CalcMoveCoeff(pickedModelDepth, viewport->width);
-        auto angleCoeff = camera->CalcAngleCoeff(viewport->width);
-        if (pickedModel) {
-            //pickedModel->SetTout(pickedToutAtPress);
-            if (buttonState[GLFW_MOUSE_BUTTON_RIGHT] != GLFW_RELEASE)
-                pickedModel->TranslateInSystem(system, {-float(xAtPress - x) / moveCoeff, float(yAtPress - y) / moveCoeff, 0});
-            if (buttonState[GLFW_MOUSE_BUTTON_MIDDLE] != GLFW_RELEASE)
-                pickedModel->RotateInSystem(system, float(xAtPress - x) / angleCoeff, Axis::Z);
-            if (buttonState[GLFW_MOUSE_BUTTON_LEFT] != GLFW_RELEASE) {
-                pickedModel->RotateInSystem(system, float(xAtPress - x) / angleCoeff, Axis::Y);
-                pickedModel->RotateInSystem(system, float(yAtPress - y) / angleCoeff, Axis::X);
-            }
-        } else {
-           // camera->SetTout(cameraToutAtPress);
-            if (buttonState[GLFW_MOUSE_BUTTON_RIGHT] != GLFW_RELEASE)
-                root->TranslateInSystem(system, {-float(xAtPress - x) / moveCoeff/10.0f, float( yAtPress - y) / moveCoeff/10.0f, 0});
-            if (buttonState[GLFW_MOUSE_BUTTON_MIDDLE] != GLFW_RELEASE)
-                root->RotateInSystem(system, float(x - xAtPress) / 180.0f, Axis::Z);
-            if (buttonState[GLFW_MOUSE_BUTTON_LEFT] != GLFW_RELEASE) {
-                root->RotateInSystem(system, float(x - xAtPress) / angleCoeff, Axis::Y);
-                root->RotateInSystem(system, float(y - yAtPress) / angleCoeff, Axis::X);
-            }
-        }
-        xAtPress =  x;
-        yAtPress =  y;
-    }
+//    if (dragging) {
+//        Eigen::Matrix3f system = camera->GetRotation().transpose() * GetRotation();
+//        auto moveCoeff = camera->CalcMoveCoeff(pickedModelDepth, viewport->width);
+//        auto angleCoeff = camera->CalcAngleCoeff(viewport->width);
+//        if (pickedModel) {
+//            //pickedModel->SetTout(pickedToutAtPress);
+//            if (buttonState[GLFW_MOUSE_BUTTON_RIGHT] != GLFW_RELEASE)
+//                pickedModel->TranslateInSystem(system, {-float(xAtPress - x) / moveCoeff, float(yAtPress - y) / moveCoeff, 0});
+//            if (buttonState[GLFW_MOUSE_BUTTON_MIDDLE] != GLFW_RELEASE)
+//                pickedModel->RotateInSystem(system, float(xAtPress - x) / angleCoeff, Axis::Z);
+//            if (buttonState[GLFW_MOUSE_BUTTON_LEFT] != GLFW_RELEASE) {
+//                pickedModel->RotateInSystem(system, float(xAtPress - x) / angleCoeff, Axis::Y);
+//                pickedModel->RotateInSystem(system, float(yAtPress - y) / angleCoeff, Axis::X);
+//            }
+//        } else {
+//           // camera->SetTout(cameraToutAtPress);
+//            if (buttonState[GLFW_MOUSE_BUTTON_RIGHT] != GLFW_RELEASE)
+//                root->TranslateInSystem(system, {-float(xAtPress - x) / moveCoeff/10.0f, float( yAtPress - y) / moveCoeff/10.0f, 0});
+//            if (buttonState[GLFW_MOUSE_BUTTON_MIDDLE] != GLFW_RELEASE)
+//                root->RotateInSystem(system, float(x - xAtPress) / 180.0f, Axis::Z);
+//            if (buttonState[GLFW_MOUSE_BUTTON_LEFT] != GLFW_RELEASE) {
+//                root->RotateInSystem(system, float(x - xAtPress) / angleCoeff, Axis::Y);
+//                root->RotateInSystem(system, float(y - yAtPress) / angleCoeff, Axis::X);
+//            }
+//        }
+//        xAtPress =  x;
+//        yAtPress =  y;
+//    }
 }
 
 void BasicScene::KeyCallback(Viewport* viewport, int x, int y, int key, int scancode, int action, int mods)
@@ -228,8 +229,8 @@ void BasicScene::KeyCallback(Viewport* viewport, int x, int y, int key, int scan
                 TurnRight();
                 break;
             case GLFW_KEY_1:
-                if( pickedIndex > 0)
-                  pickedIndex--;
+//                if( pickedIndex > 0)
+//                  pickedIndex--;
                 break;
             case GLFW_KEY_2:
 
